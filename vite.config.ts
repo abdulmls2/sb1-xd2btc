af6368ac110bd871b3f7b33b1403a36ff205c2c5
abdulmls2 <abdullah.m1230@gmail.com>
2024-12-10 22:07:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,17 +9,6 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   define: {
-    // Polyfill 'process' in the browser context
-    process: {
-      env: {
-        NODE_ENV: JSON.stringify('production'), // Ensure this is set to 'production' in production builds
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      // Ensure Vite uses the 'process' polyfill
-      process: path.resolve(__dirname, 'node_modules/process/browser'),
-    },
+    'process.env': process.env, // polyfill for process.env
   },
 });
